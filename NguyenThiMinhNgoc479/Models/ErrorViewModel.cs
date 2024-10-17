02f440a540a6e97f754d058e2684e4c824826509
ngoc0102 <ngoc0102@gmail.com>
2024-10-17 21:57:00 +0700

    Create_Project

---
+++ NguyenThiMinhNgoc479/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiMinhNgoc479.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
